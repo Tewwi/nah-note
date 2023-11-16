5f45c1ae4b399e3e768515cd3279120e34cc2fd5
NguyenIchDuy <ichduy1882001@gmail.com>
2023-11-16 16:05:46 +0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
 
   if (!token) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   return NextResponse.next();
