3eeb958ad8660232b39c03aa41b22a092b242e04
NguyenIchDuy <ichduy1882001@gmail.com>
2023-12-19 11:03:09 +0700

    refacter/clear-log-in-middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,6 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
 
-  console.log(request);
   if (!token) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
