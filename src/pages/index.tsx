cd2c23094ec30d1193df03fa08ea4f7ffcb7a8a7
NguyenIchDuy <ichduy1882001@gmail.com>
2023-12-19 09:57:54 +0700

    remove ssr in loading page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { getCookie } from "cookies-next";
 import { useRouter } from "next/router";
-import type { GetServerSideProps } from "next/types";
 import { useEffect } from "react";
 import PageLoading from "~/components/PageComponent/PageLoading";
-import { generateSSGHelper } from "~/server/utils";
 import { api } from "~/utils/api";
 import { Role } from "~/utils/constant";
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const token = getCookie("token", { req, res });
-  if (token) {
-    const ssg = generateSSGHelper(token?.toString());
-    await ssg.user.getCurrUserDetail.prefetch();
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+//   const token = getCookie("token", { req, res });
+//   if (token) {
+//     const ssg = generateSSGHelper(token?.toString());
+//     await ssg.user.getCurrUserDetail.prefetch();
 
-    return {
-      props: {
-        trpcState: ssg.dehydrate(),
-      },
-    };
-  }
+//     return {
+//       props: {
+//         trpcState: ssg.dehydrate(),
+//       },
+//     };
+//   }
 
-  return {
-    redirect: {
-      destination: "dashboard",
-      permanent: false,
-    },
-  };
-};
+//   return {
+//     redirect: {
+//       destination: "dashboard",
+//       permanent: false,
+//     },
+//   };
+// };
 
 export default function Home() {
   const { data: userPages } = api.page.getPageByCurrUser.useQuery({ page: 1 });
